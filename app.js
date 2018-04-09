0f93cd42e297c6498eb07bea4c0a200596a06d71
sakkuru <sakkuru0323@gmail.com>
2018-04-09 14:29:52 +0900

    update env val name

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const server = app.listen(port, () => {
 
 // Create chat bot
 const connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
 });
 
 const bot = new builder.UniversalBot(connector);
